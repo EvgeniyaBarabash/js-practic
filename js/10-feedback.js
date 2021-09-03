b2673cc3364652c1387bbaec954b45d40db28b57
EvgeniyaBarabash <barabash1989@gmail.com>
2021-09-03 22:42:08 +0300

    localStorage данных формы

--- js/10-feedback.js
+++ js/10-feedback.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('.js-feedback-form');
 const textarea = document.querySelector('.js-feedback-form textarea');
-const label = document.querySelector('.js-feedback-form label');
+const label = document.querySelector('.js-feedback-form input');
 
 /*
  * - Останавливаем поведение по умолчанию
@@ -8,7 +8,7 @@ const label = document.querySelector('.js-feedback-form label');
  * - Очищаем форму
  */
 form.addEventListener('submit',onFormSubmit);
-textarea.addEventListener('input', onTextareaInput);
+// textarea.addEventListener('input', onTextareaInput);
 const STORAGE_KEY = 'feadback-massage';
 function onFormSubmit(evt){
     evt.preventDefault();
@@ -21,47 +21,37 @@ function onFormSubmit(evt){
  * - Сохраняем его в хранилище
  * - Можно добавить throttle
  */
-function onTextareaInput(e){
-console.log(e.currentTarget.value);
-localStorage.setItem(STORAGE_KEY, e.currentTarget.value);
-};
+// function onTextareaInput(e){
+// console.log(e.currentTarget.value);
+// localStorage.setItem(STORAGE_KEY, e.currentTarget.value);
+// };
 /*
  * - Получаем значение из хранилища
  * - Если там что-то было, обновляем DOM
  */
-function populateTextarea(){
-const savedMassage = localStorage.getItem(STORAGE_KEY);
-if(savedMassage){
-    textarea.value=savedMassage;
-}
-};
-populateTextarea()
+// function populateTextarea(){
+// const savedMassage = localStorage.getItem(STORAGE_KEY);
+// if(savedMassage){
+//     textarea.value=savedMassage;
+// }
+// };
+// populateTextarea()
 
 // сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
-// const formData = {};
-// const STORAGE_KEY = 'massage';
-
-// form.addEventListener('input', (e)=>{
-//     // e.preventDefault();
-//     //     e.currentTarget.reset();
-      
-//     // console.log(e.target.name);
-//     // console.log(e.target.value);
-//     formData[e.target.name]=e.target.value;
-// localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// console.log(formData)
-// });
-// const saveData = localStorage.getItem(STORAGE_KEY);
-// console.log(saveData);
-// const parseData = JSON.parse(saveData);
-// console.log(parseData.name);
-// console.log(parseData.message);
-// if (saveData){
-//     textarea.textContent = parseData.message;
-//     label.value = parseData.name;
-//    };
-//    function onFormSubmit(evt){
-//     evt.preventDefault();
-//     evt.currentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-// };
+const formData = {};
+ form.addEventListener('input', onFormInput);
+function onFormInput(e){
+    formData[e.target.name]=e.target.value;
+    console.log(formData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+};
+ function getDataFromInput(){
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parseData = JSON.parse(savedData);
+    console.log(parseData);
+    if(parseData){
+        textarea.value=parseData.message;
+        label.value=parseData.name;
+    }
+ };
+ getDataFromInput();
